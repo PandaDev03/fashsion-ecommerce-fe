dc85a25d9a0673fa150290dbd296a7dbb2347de3
PandaDev03 <daiphucduongvinh203@gmail.com>
2025-11-07 21:23:58 +0700

    feat(assets): Add initial SVG icons

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,5 @@
+export { ReactComponent as ArrowDown } from './arrow-down.svg';
+export { ReactComponent as Cart } from './cart.svg';
+export { ReactComponent as EmptyCart } from './empty-cart.svg';
+export { ReactComponent as LOGO } from './logo.svg';
+export { ReactComponent as Search } from './search.svg';
